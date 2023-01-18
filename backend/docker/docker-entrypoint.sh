aed7e2d3de97aa45bf8f0ea7bac6424f227826c6
milkicloud <mika77@stafag.ch>
2023-01-18 10:16:29 +0100

    -

--- backend/docker/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /var/www/html
-rm -f composer.lock
-composer install && apache2-foreground
--- frontend/docker/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /lernApp
-rm -f package-lock.json
-rm -f yarn.lock
-npm install && ng serve
