df7cec66b2b2eb6add44003d379afb380b337e97
milkicloud <mika77@stafag.ch>
2023-01-25 07:31:03 +0100

    -

---
+++ lapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { AccountComponent } from './account/account.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    AccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ lapp/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://gltvcsimcjcrteklmmkj.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsdHZjc2ltY2pjcnRla2xtbWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxMzkxNjMsImV4cCI6MTk4OTcxNTE2M30.A7ncciXUWcjFOlBgJoVjQb7V_QP0SwiGgSVC_YRV2YM',  
+
+};
