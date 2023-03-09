9bca092b90bde7b7898366af71ded55226338a4d
RubenSouto <61864061+RubenSouto@users.noreply.github.com>
2023-03-09 14:56:02 +0100

    Funktionalität fertig + Paar Theorie-Inhalte

--- lapp/src/app/shared/hard-code-thema-data.service.ts
+++ lapp/src/app/shared/hard-code-thema-data.service.ts
@@ -11,16 +11,8 @@ export class HardCodeThemaDataService {
     this.themaList = [
       {
         id:1,
-        titel: "Thema 1"
-      },
-      {
-        id:2,
-        titel: "Thema 2"
-      },
-      {
-        id:3,
-        titel: "Thema 3"
-      }   
+        titel: "Thermodynamik"
+      }  
     ]
    }
 }
--- lapp/src/app/shared/sub-thema-data.service.ts
+++ lapp/src/app/shared/sub-thema-data.service.ts
@@ -12,32 +12,72 @@ export class SubThemaDataService {
       {     
         parentId:1,
         id:1,
-        titel: "subThema1"
+        titel: "1.1 Temperaturbegriff"
       },
       {
         parentId:1,
         id:2,
-        titel: "subThema2"
+        titel: "1.2 Messung der Temperatur"
       },
       {
-        parentId:2,
+        parentId:1,
         id:3,
-        titel: "subThema3"
+        titel: "1.3 Wärmeausdehnung"
       },
       {
-        parentId:2,
+        parentId:1,
         id:4,
-        titel: "subThema4"
+        titel: "1.3.1 Längenausdehung von Festkörpern"
       },
       {
-        parentId:3,
+        parentId:1,
         id:5,
-        titel: "subThema5"
+        titel: "1.3.2 Flächenausdehnung"
       },
       {
-        parentId:3,
+        parentId:1,
         id:6,
-        titel: "subThema6"
+        titel: "1.3.3 Volumenausdehnung"
+      },
+      {
+        parentId:1,
+        id:7,
+        titel: "1.3.3.1 Veränderung der Dichte bei Temperaturänderungen"
+      },
+      {
+        parentId:1,
+        id:8,
+        titel: "1.3.3.1 Veränderung der Dichte bei Temperaturänderungen"
+      },
+      {
+        parentId:1,
+        id:9,
+        titel: "1.4 Wärme"
+      },
+      {
+        parentId:1,
+        id:10,
+        titel: "1.4.1 Beziehung Wärme-Temperatur"
+      },
+      {
+        parentId:1,
+        id:11,
+        titel: "1.5 Phasenübergänge"
+      },
+      {
+        parentId:1,
+        id:12,
+        titel: "1.6 Hauptsätze der Thermodynamik"
+      },
+      {
+        parentId:1,
+        id:13,
+        titel: "1.7 Mischprozesse"
+      },
+      {
+        parentId:1,
+        id:14,
+        titel: "1.8 Gasgesetze für ideale Gase"
       }
     ]
   }
